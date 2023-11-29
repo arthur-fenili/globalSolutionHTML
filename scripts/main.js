ece298c85a1c614355460277a3dc5cf4c4611702
Arthur <arthur.fenili@hotmail.com>
2023-11-29 18:04:16 -0300

    Commit com página completa e início da responsividade

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-console.log('Testando!')
+function toggleTexto() {
+    var elemento = document.querySelector('.solution');
+    if (elemento.style.display === 'none' || elemento.style.display === '') {
+      elemento.style.display = 'block';
+      document.getElementById('magic-button').innerText = 'ESCONDER'
+    } else {
+      elemento.style.display = 'none';
+      document.getElementById('magic-button').innerText = 'REVELAR'
+    }
+  }
